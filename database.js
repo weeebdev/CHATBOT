ccab401c48a1bfa8cd8464b8a6b5e703efa6e218
Madiyar Mukushev <madiyar2000@gmail.com>
2020-03-15 21:40:20 +0600

    updated path

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./coroba-dbo.json");
+const serviceAccount = require("./corona-dbo.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
